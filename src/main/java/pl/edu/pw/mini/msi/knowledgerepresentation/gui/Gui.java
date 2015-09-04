a15b48c004fc2093fc2c2bdba511022e4df06329
Tomasz Janiszewski <janiszt@gmail.com>
2015-09-04 18:56:40 +0200

    Allow to edit definitions

--- src/main/java/pl/edu/pw/mini/msi/knowledgerepresentation/gui/Gui.java
+++ src/main/java/pl/edu/pw/mini/msi/knowledgerepresentation/gui/Gui.java
@@ -41,7 +41,6 @@ public class Gui extends Application {
         Label definitionsLabel = new Label("Definitions");
         definitionsTextArea = new TextArea();
         definitionsTextArea.setPrefWidth(100);
-        definitionsTextArea.setEditable(false);
         definitionsTextArea.setText(IOUtils.toString(getClass().getResource("/definition3.al")));
         VBox.setVgrow(definitionsTextArea, Priority.ALWAYS);
         HBox.setHgrow(vbox, Priority.ALWAYS);
