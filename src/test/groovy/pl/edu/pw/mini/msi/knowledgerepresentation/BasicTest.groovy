6750fde6205c072dfa945f67f4b928c67dc159de
Rafał Wyka <wykasz@gmail.com>
2015-09-07 11:45:27 +0200

    Chain of typical events

--- src/test/groovy/pl/edu/pw/mini/msi/knowledgerepresentation/BasicTest.groovy
+++ src/test/groovy/pl/edu/pw/mini/msi/knowledgerepresentation/BasicTest.groovy
@@ -112,5 +112,6 @@ class BasicTest extends Specification {
         '/8-triggerTypically'     | 5     | 'true, true, true, true, false, false, true, false, true'
         '/9-occursTypicallyQ'     | 5     | 'true, true, true, true, false, true, true, true, false, true'
         '/10-XOR_Typically'       | 5     | 'true, true, true, true, true, true, true, true, true, true'
+        '/11-typicallyInChain'    | 5     | 'true, false, true, true, true, false, false, false, true, true, true'
     }
 }
